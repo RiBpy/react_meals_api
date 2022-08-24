5b55bd5e80fd6cad597462c2fe709709227a0b63
RiBpy <77004199+RiBpy@users.noreply.github.com>
2022-08-24 21:38:46 +0600

    Updates

---
+++ precache-manifest.ea749535b78e4c66a390bb17d0fcee76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "095c097d7dbda9ea72c81bd0d65d774d",
+    "url": "/react_meals_api/index.html"
+  },
+  {
+    "revision": "aa4736586d1446178990",
+    "url": "/react_meals_api/static/css/main.57341a47.chunk.css"
+  },
+  {
+    "revision": "268ea6237f20c6eef1e7",
+    "url": "/react_meals_api/static/js/2.85b74181.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_meals_api/static/js/2.85b74181.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa4736586d1446178990",
+    "url": "/react_meals_api/static/js/main.7ba26b82.chunk.js"
+  },
+  {
+    "revision": "adcad71766dd8d803ebc",
+    "url": "/react_meals_api/static/js/runtime-main.b93b61ce.js"
+  },
+  {
+    "revision": "368e0d6f2bd633aa42789da0a947d0dc",
+    "url": "/react_meals_api/static/media/ri.368e0d6f.png"
+  }
+]);
